56abc5c70e4fc7294e48e4f0fcdb11ca9e3136b7
Alexandra Torres <artorrespino@gmail.com>
2023-08-04 13:24:00 -0500

    Iniciando FrontEnd Angular proyecto SaludNativa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListarProductoComponent } from './componentes/producto/listar-producto/listar-producto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListarProductoComponent
   ],
   imports: [
     BrowserModule,
