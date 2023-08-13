4534c60210f8b413c09b22660991fa4ef70606b9
Alexandra Torres <artorrespino@gmail.com>
2023-08-13 05:17:22 -0500

    ADD: class Cliente y clienteService .class tipo de doc

--- src/app/modelos/Cliente.ts
+++ src/app/modelos/Cliente.ts
@@ -1,4 +1,18 @@
+import { TipoDocIdentidad } from "./TipoDocIdentidad";
 
 export class Cliente{
+    idCliente?: number;
+    fecNacimiento: Date;
+    numDocIdentidad: string;
+    idUsuario: number;
+    tipoDocIdentidad: TipoDocIdentidad;
+
+    constructor(){
+        this.idCliente=0;
+        this.fecNacimiento = new Date();;
+        this.numDocIdentidad="";
+        this.idUsuario=0;
+        this.tipoDocIdentidad = new TipoDocIdentidad();
+    }
     
 }
--- src/app/servicio/cliente.service.ts
+++ src/app/servicio/cliente.service.ts
@@ -1,9 +1,48 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { Cliente } from '../modelos/Cliente';
+import { Observable } from 'rxjs';
+import { TipoDocIdentidad } from '../modelos/TipoDocIdentidad';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
-  constructor() { }
+  // Define las URLs base para tus tres microservicios
+  //private baseUrlMicroservicio1 = 'http://localhost:8080/api';
+  private baseUrlMicroservicio2 = 'http://localhost:8081/api';
+  //private baseUrlMicroservicio3 = 'http://localhost:8082/api';
+
+  constructor(private apiService: ApiService) { }
+
+  getClientes(): Observable<Cliente[]> {
+    const endpoint = 'clientes';
+    return this.apiService.get<Cliente[]>(this.baseUrlMicroservicio2, endpoint);
+  }
+
+  createCliente(usuario: Cliente): Observable<Cliente> {
+    const endpoint = 'clientes';
+    return this.apiService.post<Cliente>(this.baseUrlMicroservicio2, endpoint, usuario);
+  }
+
+  getClienteId(id: number): Observable<Cliente> {
+    const endpoint = 'clientes';
+    return this.apiService.get<Cliente>(this.baseUrlMicroservicio2, `${endpoint}/${id}`);
+  }
+
+  updateCliente(usuario: Cliente): Observable<Cliente> {
+    const endpoint = 'clientes';
+    return this.apiService.put<Cliente>(this.baseUrlMicroservicio2, endpoint, usuario);
+  }
+
+  deleteCliente(usuario: Cliente): Observable<Cliente> {
+    const endpoint = 'clientes';
+    return this.apiService.delete<Cliente>(this.baseUrlMicroservicio2, `${endpoint}/${usuario.idCliente}`);
+  }
+
+  getTiposDocIdentidad(): Observable<TipoDocIdentidad[]> {
+    const endpoint = 'tipo-doc-identidad';
+    return this.apiService.get<TipoDocIdentidad[]>(this.baseUrlMicroservicio2, endpoint);
+  }
 }
